2beca1cf3b92af6b2106087199f425eaf5c27e76
AlexBud96 <AlexSy4ov@gmail.com>
2016-09-25 18:33:31 +0000

    Исправлены имена переменных. Наибольший и наименьший бары вычисляются в одной функции

--- bars.py
+++ bars.py
@@ -11,30 +11,26 @@ def load_data(filepath):
         data = json.load(file_handler)
     return data
     
-def print_bar(barName, howBig):
-     if not howBig:
-            print ('Заведение %s находится на кратчайшем расстоянии от вас' % (barName))
+def print_bar(bar_name, how_big):
+     if not how_big:
+            print ('Заведение %s находится на кратчайшем расстоянии от вас' % (bar_name))
      else:
-            print ('В заведении %s имеется %d свободных мест' % (barName, howBig))
+            print ('В заведении %s имеется %d свободных мест' % (bar_name, how_big))
 
-def get_biggest_bar(data):
-    barName = data[1]['Cells']['Name']
+def get_big_and_small_bars(data):
+    bar_name1 = data[1]['Cells']['Name']
+    bar_name2 = data[1]['Cells']['Name']
     biggest = data[1]['Cells']['SeatsCount']
+    smallest = data[1]['Cells']['SeatsCount']
     for bar in data:
         if biggest < bar['Cells']['SeatsCount']:
             biggest = bar['Cells']['SeatsCount']
-            barName = bar['Cells']['Name']
-    print_bar(barName, biggest)
-
-
-def get_smallest_bar(data):
-    barName = data[1]['Cells']['Name']
-    smallest = data[1]['Cells']['SeatsCount']
-    for bar in data:
+            bar_name1 = bar['Cells']['Name']
         if smallest > bar['Cells']['SeatsCount']:
             smallest = bar['Cells']['SeatsCount']
-            barName = bar['Cells']['Name']
-    print_bar(barName, smallest)
+            bar_name2 = bar['Cells']['Name']
+    print_bar(bar_name1, biggest)
+    print_bar(bar_name2, smallest)
 
 def length_of_path(coordinates, longitude, latitude):
     length = math.sqrt((int(coordinates[0]) ** 2 - int(longitude) ** 2) + (int(coordinates[1]) ** 2 - int(latitude) ** 2))
@@ -53,8 +49,7 @@ def get_closest_bar(data, longitude, latitude):
 
 if __name__ == '__main__':
     data_dir = input('Укажите путь до файла: ')
-    get_biggest_bar(load_data(data_dir))
-    get_smallest_bar(load_data(data_dir))
+    get_big_and_small_bars(load_data(data_dir))
     longitude = input('Широта: ')
     latitude = input('Долгота: ')
     get_closest_bar(load_data(data_dir), longitude, latitude)
